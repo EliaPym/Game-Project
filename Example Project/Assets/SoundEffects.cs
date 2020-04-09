472bc11046b3c8b4b0720e03023444f7ea010968
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-04-09 11:41:17 +0100

    Texture Update

---
+++ Example Project/Assets/SoundEffects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip Land;
+    public AudioClip Coin;
+    public AudioClip PowerUp;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            audioSource.clip = Land;
+        }
+        else if (other.gameObject.tag == "Coin")
+        {
+            audioSource.clip = Coin;
+        }
+        else if (other.gameObject.tag == "PowerUp")
+        {
+            audioSource.clip = PowerUp;
+        }
+        audioSource.Play();
+    }
+}
