0387fb0de45623b5d1dabe3dec565849a2dc8638
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-04-09 11:18:20 +0100

    Unity Example Project Test Thing
    Example

---
+++ Example Project/Assets/DetectObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObject : MonoBehaviour
+{
+    public bool detectObject(Vector3 checkCoord,float radius)
+    {
+        Collider[] intersects = Physics.OverlapSphere(checkCoord, radius); //Gets list of objects in area
+        if (intersects.Length > 0) //If there is an item in the list of objects in the area
+                                   //the function will return true
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
